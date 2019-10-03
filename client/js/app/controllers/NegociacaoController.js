45a01086f1414b108d34524c3141665beef1dcdd
Gabriel <gabriel.developer@outlook.com>
2019-10-02 22:42:45 -0300

    adicionando rest operator

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -3,24 +3,21 @@ class NegociacaoController {
     constructor() {
         
         let $ = document.querySelector.bind(document);
+        
         this._inputData = $('#data');
         this._inputQuantidade = $('#quantidade');
         this._inputValor = $('#valor');
-        
-        this._listaNegociacoes = ProxyFactory.create(
-                new ListaNegociacoes(), 
-                ['adiciona', 'esvazia'], (model) => 
-                    this._negociacoesView.update(model));
 
-        this._negociacoesView = new NegociacoesView($('#negociacoesView'));
-        this._negociacoesView.update(this._listaNegociacoes);
+        this._listaNegociacoes = new Bind(
+            new ListaNegociacoes(),
+            new NegociacoesView($('#negociacoesView')),
+            'adiciona', 'esvazia')
         
-        this._mensagem = ProxyFactory.create(
-            new Mensagem(), ['texto'], model => 
-                this._mensagemView.update(model));
-
-        this._mensagemView = new MensagemView($('#mensagemView'));
-        this._mensagemView.update(this._mensagem);
+        this._mensagem = new Bind(
+            new Mensagem(),
+            new MensagemView($('#mensagemView')),
+            'texto');  
+        
     }
     
     adiciona(event) {
