2461772d00c9c3219720b01bdfedb1169a5cbbd0
Gabriel <gabriel.developer@outlook.com>
2019-10-06 14:01:08 -0300

    adicionando importações da semana anteriror e retrasada

--- client/js/app/services/NegociacaoService.js
+++ client/js/app/services/NegociacaoService.js
@@ -25,4 +25,56 @@ class NegocicaoService{
         };
         xhr.send();
     }
+
+    obterNegociacoesDaSemanaAnterior(cb){
+
+        let xhr = new XMLHttpRequest();
+        xhr.open('GET', 'negociacoes/anterior');
+
+        xhr.onreadystatechange = () => {
+
+            if(xhr.readyState == 4) {
+
+                if(xhr.status == 200) {
+                    console.log('Obtendo as negociações do servidor');
+
+                    cb(null, JSON.parse(xhr.responseText)
+                        .map(objeto => new Negociacao(new Date(objeto.data), objeto.quantidade, objeto.valor)));
+                                
+                } else {
+                    console.log(xhr.responseText);
+                    cb('Não foi possível obter as Negociações', null);
+                   
+                }
+
+            }
+        };
+        xhr.send();
+    }
+
+    obterNegociacoesDaSemanaRetrasada(cb){
+
+        let xhr = new XMLHttpRequest();
+        xhr.open('GET', 'negociacoes/retrasada');
+
+        xhr.onreadystatechange = () => {
+
+            if(xhr.readyState == 4) {
+
+                if(xhr.status == 200) {
+                    console.log('Obtendo as negociações do servidor');
+
+                    cb(null, JSON.parse(xhr.responseText)
+                        .map(objeto => new Negociacao(new Date(objeto.data), objeto.quantidade, objeto.valor)));
+                                
+                } else {
+                    console.log(xhr.responseText);
+                    cb('Não foi possível obter as Negociações', null);
+                   
+                }
+
+            }
+        };
+        xhr.send();
+    }
 }
