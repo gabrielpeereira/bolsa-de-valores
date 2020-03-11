f354904f90d665084ff7df4b983f5b0c038ac370
Gabriel <gabriel.developer@outlook.com>
2020-03-11 00:49:05 -0300

    restart typescript

--- client/js/app/models/Negociacao.js
+++ client/js/app/models/Negociacao.js
@@ -1,30 +1,23 @@
 class Negociacao {
-    
-    constructor(data, quantidade, valor) {
-        
-        this._data = new Date(data.getTime());
+    constructor(data, quantidade, valor){
+        this._data = data;
         this._quantidade = quantidade;
         this._valor = valor;
-        Object.freeze(this);
     }
-    
-    get volume() {
-        
-        return this._quantidade * this._valor;
+
+    get data(){
+        return this._data;
     }
-    
-    get data() {
-        
-        return new Date(this._data.getTime());
-    }
-    
-    get quantidade() {
-        
+
+    get quantidade(){
         return this._quantidade;
     }
-    
-    get valor() {
-        
+
+    get valor(){
         return this._valor;
+    }
+
+    get volume(){
+        return this._quantidade * this._valor;
     }
 }
